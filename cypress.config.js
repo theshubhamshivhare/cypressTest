8512c39a0345df3246dfe2f00cf2d51883c05b06
Shubham Shivhare <shubham7047@gmail.com>
2024-03-26 09:25:30 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "bvnico",
   reporter: 'cypress-mochawesome-reporter',
   chromeWebSecurity: false,
   video: true,
